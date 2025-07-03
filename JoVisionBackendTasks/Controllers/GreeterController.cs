86ddd6c0d0abb313ae8972597522dc7d3c5e286d
2305saja <sajamar1712005@gmail.com>
2025-07-03 11:19:07 +0300

    Initial ASP.NET Web API with GreeterController

---
+++ JoVisionBackendTasks/Controllers/GreeterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JoVisionBackendTasks.Controllers  
+{
+    [ApiController]
+    [Route("api/[controller]")]       
+    public class GreeterController : ControllerBase
+    {
+       
+        [HttpGet]
+        public IActionResult Get([FromQuery] string? name)
+        {
+            string finalName = string.IsNullOrWhiteSpace(name) ? "anonymous" : name;
+            return Ok($"Hello {finalName}");
+        }
+    }
+}
